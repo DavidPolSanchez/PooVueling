fd8721c21533bd99359c2272e496b81bde0a3596
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2018-06-21 11:44:22 +0200

    comiteando proyecto calculadora

---
+++ PooVueling.Integration.Tests/CalculadoraTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PooVueling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVueling.Tests
+{
+    [TestClass()]
+    public class CalculadoraTests
+    {
+        [TestMethod()]
+        public void CloneTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
